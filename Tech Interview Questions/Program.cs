f925c1484cefaa2052b211a93c854914789f2802
bradberger01 <bradberger@hotmail.com>
2017-09-12 19:46:31 -0400

    More Practices

--- Tech Interview Questions/Program.cs
+++ Tech Interview Questions/Program.cs
@@ -10,32 +10,70 @@ namespace Tech_Interview_Questions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me a number!");
-            int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Give me a number!");
+            //int number = int.Parse(Console.ReadLine());
 
-            int divByThree = number % 3;
-            int divByFive = number % 5;
+            //int divByThree = number % 3;
+            //int divByFive = number % 5;
 
-            if (divByFive == 0 && divByThree == 0)
+            //if (divByFive == 0 && divByThree == 0)
+            //{
+            //    Console.WriteLine("Fizz Buzz");
+            //  }
+            //else if (divByFive == 0)
+            //{
+            //    Console.WriteLine("Buzz");
+            //}
+            //else if (divByThree == 0)
+            //{i
+
+            //    Console.WriteLine("Fizz");             
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This number is not divisible by 3 or 5");
+            //}
+
+            //print out all prime numbers that are less than 100 to the console. 0 and 1 are not prime numbers
+
+            List<int> oneToHundred = new List<int> { 3, 5, 7 };
+            Console.WriteLine(3);
+            Console.WriteLine(5);
+            Console.WriteLine(7);
+            for(int i =2; i<=100; i++)
+              
             {
-                Console.WriteLine("Fizz Buzz");
-              }
-            else if (divByFive == 0)
-            {
-                Console.WriteLine("Buzz");
+                if(i % 2 != 0 && i % 3 !=0 && i % 7 !=0 && i % 5 !=0)
+                {
+                    
+                    Console.WriteLine(i);
+                }
             }
-            else if (divByThree == 0)
+            //OR
+
+            bool isPrime = true;
+
+            for(int i =2; i <= 100; i++)
             {
-                Console.WriteLine("Fizz");             
+                for(int j = 2; j < 100; j++)
+                {
+                    if(i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write(i + " ");
+                }
+                isPrime = true;
+
             }
-            else
-            {
-                Console.WriteLine("This number is not divisible by 3 or 5");
-            }
 
 
 
-            
+
         }
     }
 }
